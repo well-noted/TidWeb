0820bd0de0b92907f537409b1860b2c62318588b
Thomas E Tuoti <tetuoti@gmail.com>
2025-03-17 07:56:14 -0700

    Initial Push

--- app/src/main/java/com/tiddlywikibrowser/model/TiddlerTemplate.kt
+++
@@ -1,8 +0,0 @@
-package com.tiddlywikibrowser.model
-
-data class TiddlerTemplate(
-    val name: String,
-    val fileName: String,
-    val description: String? = null,
-    val content: String? = null
-)
--- src/test/java/com/tiddlywikibrowser/MainActivityTest.kt
+++
@@ -1,9 +0,0 @@
-import org.junit.Assert.assertEquals
-import org.junit.Test
-
-class MainActivityTest {
-    @Test
-    fun helloWorld() {
-        assertEquals(1 + 1, 2)
-    }
-}
